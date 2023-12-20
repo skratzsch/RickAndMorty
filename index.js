f45aac02b1de16ac7cf3593f6a5da2e9312034cb
skratzsch <skratzsch@yahoo.de>
2023-12-20 18:55:08 +0100

    add forEach to append all 20 cards to cardContainer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { CharacterCard } from './js/CharacterCard.js';
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -8,7 +10,30 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const characterURL = 'https://rickandmortyapi.com/api/character/';
+
 // States
-const maxPage = 1;
+const maxPage = 20;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+
+  try {
+    const response = await fetch(characterURL);
+    const data = await response.json();
+    const characters = data.results;
+    console.log("Charaters", characters);
+
+    characters.forEach(character => {
+      const card = CharacterCard(character);
+      cardContainer.appendChild(card);
+      console.log("Card aus dem forEach", card);
+    });
+
+  } catch (error) {
+    console.error(error)
+  }
+} 
+
+fetchCharacters();
