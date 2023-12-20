309d4f48fd8048f6f58667230c4eae6e55631286
skratzsch <skratzsch@yahoo.de>
2023-12-20 19:23:58 +0100

    add eventHandler for previous and next button

--- index.js
+++ index.js
@@ -10,30 +10,51 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-const characterURL = 'https://rickandmortyapi.com/api/character/';
+const characterURL = 'https://rickandmortyapi.com/api/character';
+
+prevButton.addEventListener('click', () => {
+  if (page > 1) {
+    console.log("page", page);
+    page--;
+    fetchCharacters();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (page < maxPage) {
+    page++;
+    console.log("page", page);
+    fetchCharacters();
+  }
+});
+
 
 // States
-const maxPage = 20;
-const page = 1;
+const maxPage = 48;
+let page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
 
   try {
-    const response = await fetch(characterURL);
+    const response = await fetch(`${characterURL}?page=${page}`);
+    console.log("page-url", `${characterURL}?page=${page}`)
     const data = await response.json();
     const characters = data.results;
-    console.log("Charaters", characters);
+    //console.log("Charaters", characters);
+
+    cardContainer.innerHTML = "";
 
     characters.forEach(character => {
       const card = CharacterCard(character);
       cardContainer.appendChild(card);
-      console.log("Card aus dem forEach", card);
+      //console.log("Card aus dem forEach", card);
     });
 
   } catch (error) {
     console.error(error)
   }
 } 
+
 
 fetchCharacters();
