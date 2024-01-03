e6af17473bafda972cfb4e6da36c81ee9e4f2169
skratzsch <skratzsch@yahoo.de>
2024-01-03 23:45:36 +0100

    add step 5 of Recap Project - Refactoring

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { CharacterCard } from './js/CharacterCard.js';
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import NavButton from "./components/NavButton/NavButton.js";
+import NavPagination from "./components/NavPagination/NavPagination.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
 
 const characterURL = 'https://rickandmortyapi.com/api/character';
 
@@ -17,7 +17,17 @@ let maxPage = 42;
 let page = 1;
 let searchQuery = "";
 
-prevButton.addEventListener('click', () => {
+const searchBar = SearchBar((event) => {
+  event.preventDefault();
+  searchQuery = event.target.elements.query.value;
+  page = 1;
+  fetchCharacters();
+});
+
+searchBarContainer.append(searchBar);
+
+
+const prevButton = NavButton("prev", () => {
   if (page > 1) {
     //console.log("page", page);
     page--;
@@ -25,7 +35,7 @@ prevButton.addEventListener('click', () => {
   }
 });
 
-nextButton.addEventListener('click', () => {
+const nextButton = NavButton("next", () => {
   if (page < maxPage) {
     page++;
     //console.log("page", page);
@@ -43,6 +53,10 @@ searchBar.addEventListener('submit', (event) => {
   page = 1;
   fetchCharacters();
 });
+
+const pagination = NavPagination();
+
+navigation.append(prevButton, pagination, nextButton);
 
 async function fetchCharacters() {
 
